aa68dcc00f24bc4ed9e79293d565a267f0242370
Conor Thomason <conorth@live.unc.edu>
2021-05-02 01:35:47 -0400

    User system backend appears to be working, will work on the frontend tomorrow... hopefully.

--- server/controllers/ctrl.js
+++ server/controllers/ctrl.js
@@ -69,6 +69,66 @@ updateUser = async (req, res) => {
 	})
 }
 
+deleteUser = async (req, res) => {
+	let foundUser;
+	await User.findOne({_id: req.params.id}, (err, user) => {
+		if (err) {
+			return res.status(400).json({success: false, error: err})
+		}
+
+		if (!user) {
+			return res
+				.status(404)
+				.json({success: false, error: 'User not found'})
+		}
+		else {
+			foundUser = user;
+		}
+	}).catch(err => console.log(err))
+	if (foundUser) {
+		await User.findOneAndDelete({_id: req.params.id}, (err, user) => {
+			if (err) {
+				return res.status(400).json({success: false, error: err})
+			}
+
+			return res.status(200).json({success: true, data: foundUser})
+		}).catch(err => console.log(err))
+	}
+}
+
+getUserById = async (req, res) => {
+	await User.findOne({_id: req.params.id}, (err, user) => {
+		if (err) {
+			return res.status(400).json({success: false, error: err})
+		}
+
+		if (!user) {
+			return res
+				.status(404)
+				.json({success: false, error: 'User not found'})
+		}
+		return res.status(200).json({success: true, data: user})
+	}).catch(err => console.log(err))
+}
+
+getUsers = async(req, res) => {
+	await User.find({}, (err, users) => {
+		if (err) {
+			return res.status(400).json({success: false, error: err})
+		}
+		if (!users.length) {
+			return res
+				.status(404)
+				.json({success: false, error: 'Users not found'})
+		}
+		return res.status(200).json({success: true, data: users})
+	}).catch(err => console.log(err))
+}
+
 module.exports = {
 	createUser,
+	updateUser,
+	deleteUser,
+	getUserById,
+	getUsers,
 }
--- server/routes/router.js
+++ server/routes/router.js
@@ -5,5 +5,9 @@ const UserCtrl = require('../controllers/ctrl')
 const router = express.Router()
 
 router.post('/users', UserCtrl.createUser)
+router.delete('/users/:id', UserCtrl.deleteUser)
+router.put('/users/:id', UserCtrl.updateUser)
+router.get('/users/:id', UserCtrl.getUserById)
+router.get('/users', UserCtrl.getUsers)
 
 module.exports = router
